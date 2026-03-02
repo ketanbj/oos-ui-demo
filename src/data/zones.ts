dd89bf7d35c37f0a392776c8b976233f1267c025
Ketan Bhardwaj <ketanbj@cc.gatech.edu>
2026-03-02 15:07:04 -0500

    removed old code

--- src/data/zones.ts
+++
@@ -1,5 +0,0 @@
-export const kriosZones = [
-  { id: 'americas', anchor: 'Seattle', maxMs: 60 },
-  { id: 'emea', anchor: 'London', maxMs: 70 },
-  { id: 'apac', anchor: 'Singapore', maxMs: 80 },
-]
